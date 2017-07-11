aa988a5aff3612ba0bd85aef72e600d6214f4c20
Remi200 <ponche62880@gmail.com>
2017-07-11 14:43:25 +0200

    create function placementBombe and InstallationDesBombes

--- js/game.js
+++ js/game.js
@@ -45,7 +45,26 @@ function drawMap()
 			var celluleMap = document.createElement("td") ; 
 			// ajoute de class
 			celluleMap.classList.add("r" + i + "c" + j);
-			celluleMap.textContent = mapGame[i][j].nbBombVoisine ; 
+			
+			if(mapGame[i][j].hide)
+			{
+				// on cache la cellule
+				celluleMap.classList.add("hide");
+			}
+			else
+			{
+				// on affiche la cellule 
+				if(mapGame[i][j].isBomb)
+				{
+					//Boum
+					alert("vous avez perdu!!!") ;
+				}
+				else
+				{
+					celluleMap.textContent = mapGame[i][j].nbBombVoisine ; 
+				}
+			}
+			 
 			
 			rowArray.appendChild(celluleMap) ; 
 			
@@ -55,7 +74,52 @@ function drawMap()
 	
 }
 
-function placementBomb(nbBomb)
+function installationDesBombes(nbBomb, espace)
 {
-	
+	for(x = 0 ; x < nbBomb ; x++)
+	{
+		var spaceBomb = Math.floor(Math.random() * espace) ; 
+		
+		//on parcours le tableau 
+		for(i = 0; i < mapGame.length ; i++)
+		{
+			for(j = 0; j < mapGame[i].length; j++)
+			{
+				spaceBomb-- ; 
+				if(spaceBomb <= 0)
+				{
+					//Placement de la bombe
+					if(!placementBombe(i, j)
+						x-- ; 
+					
+					//tirage d'un nouveau nombre
+					spaceBomb = Math.floor(Math.random() * espace) ;
+				}
+			}
+		}
+	}
 }
+
+function placementBombe(i , j)
+{
+	if(!mapGame[i][j].isBomb)
+	{
+		mapGame[i][j].isBomb = true ; 
+	
+		// incrémentation des indicateur 
+		for(x = -1 ; x < 2 ; x++)
+		{
+			for(y = -1; y < 2 ; y++)
+			{
+				mapGame[i + x][j + y].nbBombVoisine++
+			}
+		}
+		return true ; 
+	}
+	else
+		return false ; 
+
+
+// appel de test 
+createMap();
+//drawMap() ; 
